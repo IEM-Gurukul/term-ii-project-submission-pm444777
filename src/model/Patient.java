ddca6cb33dfa5d922b7e4969e2de92376a260e88
pm444777 <parbonimitra777@gmail.com>
2026-03-17 11:00:26 +0530

    Refactored Person to abstract class and improved Patient implementation

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -5,12 +5,14 @@ public class Patient extends Person {
     private String patientId;
     private String disease;
 
+    // Constructor
     public Patient(String name, int age, String patientId, String disease) {
         super(name, age);
         this.patientId = patientId;
         this.disease = disease;
     }
 
+    // Getters
     public String getPatientId() {
         return patientId;
     }
@@ -19,9 +21,12 @@ public class Patient extends Person {
         return disease;
     }
 
+    // Overriding abstract method
     @Override
     public void displayInfo() {
-        super.displayInfo();
+        System.out.println("Patient Details:");
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
         System.out.println("Patient ID: " + patientId);
         System.out.println("Disease: " + disease);
     }
